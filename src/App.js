5e3fbdbb80ad81c1f1266fc634ecec1cc532e1a1
정예찬 <jeong-yechan@jeong-yechan-ui-MacBookAir.local>
2021-10-19 00:40:36 +0900

    DomRefStudy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from "react";
-import EventPracticeProto from "./EventPracticeProto";
-
+import React, {Component} from "react";
+import ScrollBox from "./ScrollBox";
 
 //import MyComponent from "./MyComponent";
 //import Counter from "./Counter";
 //import Say from "./Say";
 //import EventPractice from "./EventPractice";
 //import EventPracticeClass from "./EventPracticeClass";
-const App = () => {
-  return (
-  //<MyComponent name={"혜원아 사랑해"} 
-  //favoriteNumber={1}>
-  //  리액트</MyComponent>,
-  //  <Counter />,
-  //  <Say />,
-  //<EventPracticeClass />
-  //<EventPractice />
-  <EventPracticeProto />
-  );
-};
+//import EventPracticeProto from "./EventPracticeProto";
+//import VaildationSample from "./VaildationSample"; 
+
+
+//const App = () => { 함수형
+  class App extends Component{
+    render(){
+    return (
+    //<MyComponent name={"혜원아 사랑해"} 
+    //favoriteNumber={1}>
+    //  리액트</MyComponent>,
+    //  <Counter />,
+    //  <Say />,
+    //<EventPracticeClass />
+    //<EventPractice />
+    //<EventPracticeProto />
+    //<VaildationSample />
+    /*<div>
+      <ScrollBox ref={(ref) => this.scrollBox=ref}/>
+    </div>*/
+      <div>
+        <ScrollBox ref={(ref) => this.scrollBox=ref}/>
+        <button onClick={()=> this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+      </div>
+      );
+    };
+  }
 
 export default App;
